835073440a772df92b33e81efb1b8e397e85fad9
souvik <souvik.hajra@xigmapro.com>
2022-07-22 11:48:14 +0530

    souvik 3

--- middleware/front/AuthNotCheck.js
+++ middleware/front/AuthNotCheck.js
@@ -1,7 +1,7 @@
 function authNotUser(req, res, next) {
   if (req.cookies && req.cookies.token) {
     console.log('auth not check',req.cookies.token);
-    return res.redirect('/');
+   // return res.redirect('/');
   }
   next();
 }
