55321f0f6ea46048e8fd874dd47b7355400161a3
souvik <suvadipmaiti5@gmail.com>
2022-09-14 15:01:54 +0530

    suva 14 09 22 part 1

--- controllers/front/homeController.js
+++ controllers/front/homeController.js
@@ -4,20 +4,41 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Models = require('../../models');
 
-function home(req, res) {
-  console.log(loginAuthCheck);
+async function home(req, res) {
+  var sliders = await Models.Slider.findAll({
+    where: { status: 'Y' },
+    order: [['id', 'DESC']],
+  });
+  var features = await Models.Feature.findAll({
+    where: { status: 'Y' },
+    order: [['id', 'DESC']],
+  });
+  var designs = await Models.Design.findAll({
+    where: { status: 'Y' },
+    order: [['id', 'DESC']],
+  });
+  var testimonials = await Models.Testimonial.findAll({
+    where: { status: 'Y' },
+    order: [['id', 'DESC']],
+  });
+  var qnas = await Models.Qna.findAll({
+    where: { status: 'Y' },
+    order: [['id', 'DESC']],
+  });
   return res.render('front/pages/Home/home', {
     page_name: 'home',
+    qnas: qnas,
+    features: features,
+    designs: designs,
+    testimonials: testimonials,
+    sliders: sliders
   });
-  //console.log('home tok: ',req.cookies.token);
-  //var usertoken = req.cookies.token;
-  
 }
 
-function loaderFunc(req,res){
+function loaderFunc(req, res) {
   return res.render('front/pages/Home/loader', {
     page_name: 'loader',
-    layout:false,
+    layout: false,
   });
 }
 
