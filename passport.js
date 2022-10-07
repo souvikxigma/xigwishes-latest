f6fb40aa76121089db90a75398a5f17e0f38cc32
souvik <souvik.hajra@xigmapro.com>
2022-10-07 17:39:59 +0530

    souvik 7th oct 4th

--- passport.js
+++ passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
-const {ADMINPATH} = require('./config/path.config.js');
+const {ADMINPATH,BASEPATH} = require('./config/path.config.js');
 
   
 passport.serializeUser((user , done) => {
@@ -14,7 +14,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID:"1036617403093-i6bofotdk0s8fhhg4mmo3l0n51kt7t7t.apps.googleusercontent.com", 
     clientSecret:"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", 
-    callbackURL:`${ADMINPATH}/auth/google/callback`,
+    callbackURL:`${BASEPATH}/auth/google/callback`,
     passReqToCallback:true
   },
   function(request, accessToken, refreshToken, profile, done) {
@@ -25,7 +25,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
   clientID: '829192618465577',
   clientSecret: '9cbf894a424af221a5eae7aaf920ab7e',
-  callbackURL: `${ADMINPATH}/auth/facebook/callback`
+  callbackURL: `${BASEPATH}/auth/facebook/callback`
 }, function (accessToken, refreshToken, profile, done) {
   return done(null, profile);
 }
